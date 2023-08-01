64573b50e33349b343b30d0903da56fbd1f9ecbf
snotrman <siafrontera1@gmail.com>
2023-08-01 09:19:46 +0300

    update packagaes

--- src/templates/Page.jsx
+++ src/templates/Page.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
-import { builder, BuilderComponent } from '@builder.io/gatsby';
+import { builder, BuilderComponent } from '@builder.io/react';
 
 // Initialize the Builder SDK with your organization's API Key
 // Find the API Key on: https://builder.io/account/settings
@@ -10,23 +10,22 @@ builder.init('746be61c2558434bb2a8fa489a36b0b1')
 function PageTemplate({ data }) {
   const models = data?.allBuilderModels;
   const page = models.page?.content;
-  if (!Builder.isEditing && !Builder.isPreviewing && !props.builderContent) {
-    return <Your404Page />
- }
-  return <BuilderComponent name="page" model="page" content={props.builderContent} />;
+
+  return <BuilderComponent model="page" content={page} />;
 }
+
+export default PageTemplate;
 
 // pageQuery is a GraphQL query that 
 // fetches each page's content from Builder. 
 // Your content is rendered within the 
 // PageTemplate using BuilderComponent, provided by Builder's SDK.
 export const pageQuery = graphql`
-  query ($path: String) {
+  query ($path: String!) {
     allBuilderModels {
-      page(target: { urlPath: $path }, limit: 1, options: { cachebust: true }) {
+      page(target: { urlPath: $path }) {
         content
       }
     }
   }
 `;
-export default PageTemplate;
