86221519c3b78dff3adcd17bb4f77ad1cd9ba8d1
snotrman <siafrontera1@gmail.com>
2023-08-28 13:31:42 +0300

    changed website name back

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -13,8 +13,6 @@ import {
   HeadingSmallWithTip,
   SectionTitle,
 } from '../components/Layout/sharedStyles/headingStyles';
-import { builder, BuilderComponent } from '@builder.io/react'
-import '@builder.io/widgets';
 
 builder.init('746be61c2558434bb2a8fa489a36b0b1')
 
